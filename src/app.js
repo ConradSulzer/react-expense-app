2d50fc66f24b7cf9d57f6a3cd3c1e78ae9a01755
Conrad Sulzer <sulzer.conrad@gmail.com>
2020-08-11 10:50:55 -0500

    cleaned up app.js dummy data

--- src/app.js
+++ src/app.js
@@ -5,21 +5,8 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { addExpense, removeExpense, editExpense } from './actions/expenses.actions';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters.actions';
-import getVisibleExpenses from './selectors/expenses.selectors';
 
 const store = configureStore();
-
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-})
-
-store.dispatch(addExpense({description: 'water bill', amount: 150}));
-store.dispatch(addExpense({description: 'gas bill', amount: 10, createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 1095}));
 
 const jsx = (
     <Provider store={store}>
