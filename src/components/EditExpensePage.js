6c57fa77e77618064650c33ee1ab0559341dda1b
Conrad Sulzer <sulzer.conrad@gmail.com>
2020-08-20 16:51:14 -0500

    added edit expense to firebase

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpense } from '../actions/expenses.actions';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses.actions';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.editExpense(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
 
@@ -27,7 +27,6 @@ export class EditExpensePage extends React.Component {
     }
 };
 
-
 const mapStateToProps = (state, props) => {
     return {
         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
@@ -36,7 +35,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch, props) => {
     return {
-        editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+        startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
         startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
     }
 };
