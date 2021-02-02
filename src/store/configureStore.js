680641cf10bcbca69d1e2d7b188c119e21ad90b6
Conrad Sulzer <conrad.sulzer@gmail.com>
2021-02-02 09:50:16 -0600

    redux devtool change

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses.reducer';
 import filtersReducer from '../reducers/filters.reducer';
 import authReducer from '../reducers/auth.reducer';
 
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
 
 export default () => {
     const store = createStore(
